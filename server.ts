d0fb8456ab8e1ac65e903d99f687f6472af7ca8b
scaleupmidul <scaleup.midul@gmail.com>
2026-08-04 05:31:46 +0600

    Update server.ts

--- server.ts
+++ server.ts
@@ -10,15 +10,15 @@ import { createServer } from "http";
 import { Server } from "socket.io";
 import { createServer as createViteServer } from "vite";
 import { GoogleGenAI, Type } from "@google/genai";
-import { db } from "./server/db.js";
-import { Role } from "./src/types.js";
+import { db } from "./server/db";
+import { Role } from "./src/types";
 import { 
   metaState, 
   webhookLogs, 
   setIoInstance, 
   triggerInboundMetaWebhook, 
   RedisQueue 
-} from "./server/metaIntegration.js";
+} from "./server/metaIntegration";
 
 dotenv.config();
 
